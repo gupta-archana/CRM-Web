f91081136d348a886ced712ac2d9872cfb003bab
Deepak Yadav <yadav.deepak343@gmail.com>
2020-10-22 19:41:42 +0530

    Fix of Date picker

--- src/app/customUI/dialogs/edit-objective/edit-objective.component.ts
+++ src/app/customUI/dialogs/edit-objective/edit-objective.component.ts
@@ -6,6 +6,8 @@ import { Constants } from '../../../Constants/Constants';
 import { ObjectiveModel } from '../../../models/objective-model';
 import { ApiHandlerService } from '../../../utils/api-handler.service';
 import { ApiResponseCallback } from '../../../Interfaces/ApiResponseCallback';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-edit-objective',
@@ -16,14 +18,19 @@ export class EditObjectiveComponent implements OnInit, ApiResponseCallback {
 
 
   objectiveModel: ObjectiveModel;
+  dueDate: any;
   constructor(public dialogRef: MatDialogRef<EditObjectiveComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private apiHandler: ApiHandlerService) { }
 
   ngOnInit() {
     this.objectiveModel = JSON.parse(this.data.message);
+    console.log(this.objectiveModel)
+   this.dueDate = new Date(this.objectiveModel.dueDate).toISOString();
+   console.log(this.dueDate)
   }
   onSaveChangesClick() {
+    this.objectiveModel.dueDate = this.dueDate;
     this.apiHandler.updateObjective(createRequestJson(this), this);
   }
   onCancelClick() {
